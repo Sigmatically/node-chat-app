25f0bc0ccb27c5a85a2f59e21b4905ea0f6625bc
Jackson Pranica <jpranica@gmail.com>
2017-09-12 11:10:36 -0500

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const publicpath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+let app = express();
+
+app.use(express.static(publicpath));
+
+app.listen(port, () => {
+  console.log(`Server is up and running on port ${port}`)
+});
