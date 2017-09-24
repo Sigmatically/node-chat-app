4f9c9af57fac4c94f2c918fe0d2190c2809fc82b
Jackson Pranica <jpranica@gmail.com>
2017-09-23 19:18:15 -0500

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -30,11 +30,13 @@ socket.on('newLocationMessage', function (message) {
 $('#message-form').on('submit', function (e) {
   e.preventDefault();
 
+  var messageTextbox = $('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: $('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-
+    messageTextbox.val('');
   });
 });
 
@@ -45,13 +47,16 @@ locationButton.on('click', function (e) {
     return alert('Geolocation not supported on your browser');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location.....');
+
   navigator.geolocation.getCurrentPosition(function (position) {
-    console.log(position);
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location.');
   });
 });
